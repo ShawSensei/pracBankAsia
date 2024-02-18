3f22cf1875f891d46d43a9282ffd43b2efe68e3c
ShawSensei <shawkatwasit106@gmail.com>
2024-02-18 11:11:56 +0600

    "update"

---
+++ app/src/main/java/com/wasit/pracbankasia/CardCreateSuccessActivity.kt
@@ -0,0 +1,11 @@
+package com.wasit.pracbankasia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardCreateSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_create_success)
+    }
+}
--- app/src/main/java/com/wasit/pracbankasia/MainActivity.kt
+++ app/src/main/java/com/wasit/pracbankasia/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.wasit.pracbankasia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wasit.pracbankasia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStarted.setOnClickListener {
+            intent = Intent(this, CardApplicationActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
